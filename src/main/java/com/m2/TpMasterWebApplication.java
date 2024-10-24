26f9d588000e129fb1a780ecf872031413c1a233
khalil trabelsi <khaliltrabelsi2107@gmail.com>
2024-10-24 09:46:15 +0200

    Initial commit

---
+++ src/main/java/com/m2/TpMasterWebApplication.java
@@ -0,0 +1,13 @@
+package com.m2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpMasterWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpMasterWebApplication.class, args);
+    }
+
+}
